7f847833ae8cb391ec45c2438091831aa6126479
Samuel <samueltomar@hotmail.com>
2025-01-12 22:44:40 +0100

    Added icons for floating nav

--- src/components/FloatingNav.tsx
+++ src/components/FloatingNav.tsx
@@ -2,6 +2,8 @@
 import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 import { useState } from "react";
 import { TextFlip } from "./TextFlip";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import GravityIcon from "./GravityIcon";
 
 export const FloatingNav = () => {
   const { scrollY } = useScroll();
@@ -13,31 +15,57 @@ export const FloatingNav = () => {
   });
 
   return (
-    <motion.div
-      initial={{ y: -100, opacity: 0 }}
-      animate={{ 
-        y: visible ? 0 : -100,
-        opacity: visible ? 1 : 0
-      }}
-      transition={{
-        duration: 0.2
-      }}
-      className="fixed top-12 right-6 xl:right-12 2xl:right-16 z-50 hidden lg:block"
-    >
-      <nav className="flex flex-col gap-3 capitalize text-right text-neutral-500 text-sm xl:text-base">
-        <TextFlip className="font-bold" href="#about">
-          About
-        </TextFlip>
-        <TextFlip className="font-bold" href="#experience">
-          Experience
-        </TextFlip>
-        <TextFlip className="font-bold" href="#projects">
-          Projects
-        </TextFlip>
-        <TextFlip className="font-bold" href="#contact">
-          Contact
-        </TextFlip>
-      </nav>
-    </motion.div>
+    <>
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ 
+          y: visible ? 0 : -100,
+          opacity: visible ? 1 : 0
+        }}
+        transition={{
+          duration: 0.2
+        }}
+        className="fixed top-12 right-6 xl:right-12 2xl:right-16 z-50 hidden lg:block"
+      >
+        <nav className="flex flex-col gap-3 capitalize text-right text-neutral-500 text-sm xl:text-base">
+          <TextFlip className="font-bold" href="#about">
+            About
+          </TextFlip>
+          <TextFlip className="font-bold" href="#experience">
+            Experience
+          </TextFlip>
+          <TextFlip className="font-bold" href="#projects">
+            Projects
+          </TextFlip>
+          <TextFlip className="font-bold" href="#contact">
+            Contact
+          </TextFlip>
+        </nav>
+      </motion.div>
+
+      {/* Social Icons */}
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ 
+          y: visible ? 0 : 100,
+          opacity: visible ? 1 : 0
+        }}
+        transition={{
+          duration: 0.2
+        }}
+        className="fixed bottom-12 right-6 xl:right-12 2xl:right-16 z-50 hidden lg:block"
+      >
+        <div className="flex flex-col gap-8 text-neutral-500">
+          <GravityIcon 
+            icon={<FaGithub size={25} />}
+            href="https://github.com/devStr0ke"
+          />
+          <GravityIcon 
+            icon={<FaLinkedin size={25} />}
+            href="https://www.linkedin.com/in/samuel-c-293984212/"
+          />
+        </div>
+      </motion.div>
+    </>
   );
 };
