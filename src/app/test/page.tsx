05c3e70ade651d81143656f140de72c312065f5f
Samuel <samueltomar@hotmail.com>
2025-01-15 02:44:50 +0100

    test page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Home() {
+
+  return (
+    <>
+      <div className="w-full h-[100vh] flex justify-center items-center">
+        <h1 className="text-4xl font-bold text-center text-white">Hello</h1>
+      </div>
+    </>
+  );
+}
