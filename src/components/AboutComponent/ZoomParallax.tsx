6cacf3d1a3c8145396b6d29ed66869a7535d0d21
Samuel <samueltomar@hotmail.com>
2025-01-26 03:05:46 +0100

    fix (ZoomParallax) : colored on hover working even during scale

--- src/components/AboutComponent/ZoomParallax.tsx
+++ src/components/AboutComponent/ZoomParallax.tsx
@@ -3,168 +3,187 @@ import Image from 'next/image';
 import { useScroll, useTransform, motion } from 'framer-motion';
 import { useRef, useState } from 'react';
 
-interface Picture {
-    src: string;
-    bwSrc: string;
-    scale: any;
-    className: string;
-    sizes: string;
-}
-
 export default function ZoomParallax() {
     const container = useRef(null);
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+    
     const { scrollYProgress } = useScroll({
         target: container,
         offset: ['start start', 'end end']
-    })
+    });
 
-    const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4]);
-    const scale5 = useTransform(scrollYProgress, [0, 1], [1, 5]);
-    const scale6 = useTransform(scrollYProgress, [0, 1], [1, 6]);
-    const scale8 = useTransform(scrollYProgress, [0, 1], [1, 8]);
-    const scale9 = useTransform(scrollYProgress, [0, 1], [1, 9]);
+    const scale = useTransform(scrollYProgress, [0, 1], [1, 4]);
 
-    const pictures: Picture[] = [
+    const images = [
         {
-            src: '/about/colored/fightHexa.png',
-            bwSrc: '/about/bw/fightHexa.png',
-            scale: scale4,
-            className: 'w-[25vw] h-[25vh]',
+            bw: '/about/bw/fightHexa.png',
+            color: '/about/colored/fightHexa.png',
+            position: {
+                left: '50%',
+                top: '50%',
+                transform: 'translate(-50%, -50%)'
+            },
+            size: 'w-[25vw] h-[25vh]',
             sizes: '(max-width: 2048px) 100vw, 35vw'
         },
         {
-            src: '/about/colored/limouxFight.png',
-            bwSrc: '/about/bw/limouxFight.png',
-            scale: scale5,
-            className: 'w-[35vw] h-[30vh] -top-[30vh] left-[5vw]',
+            bw: '/about/bw/limouxFight.png',
+            color: '/about/colored/limouxFight.png',
+            position: {
+                left: '55%',
+                top: '20%',
+                transform: 'translate(-50%, -50%)'
+            },
+            size: 'w-[35vw] h-[30vh]',
             sizes: '(max-width: 2048px) 100vw, 35vw'
         },
         {
-            src: '/about/colored/hercules.png',
-            bwSrc: '/about/bw/hercules.png',
-            scale: scale6,
-            className: 'w-[20vw] h-[45vh] -top-[10vh] -left-[24vw]',
+            bw: '/about/bw/hercules.png',
+            color: '/about/colored/hercules.png',
+            position: {
+                left: '26%',
+                top: '40%',
+                transform: 'translate(-50%, -50%)'
+            },
+            size: 'w-[20vw] h-[45vh]',
             sizes: '(max-width: 1011px) 100vw, 35vw'
         },
         {
-            src: '/about/colored/bangkok.png',
-            bwSrc: '/about/bw/bangkok.png',
-            scale: scale5,
-            className: 'w-[15vw] h-[25vh] left-[21.5vw]',
+            bw: '/about/bw/bangkok.png',
+            color: '/about/colored/bangkok.png',
+            position: {
+                left: '71.5%',
+                top: '50%',
+                transform: 'translate(-50%, -50%)'
+            },
+            size: 'w-[15vw] h-[25vh]',
             sizes: '(max-width: 1011px) 100vw, 35vw'
         },
         {
-            src: '/about/colored/coffee.png',
-            bwSrc: '/about/bw/coffee.png',
-            scale: scale6,
-            className: 'w-[15vw] h-[30vh] top-[29.5vh] -left-[2vw]',
+            bw: '/about/bw/coffee.png',
+            color: '/about/colored/coffee.png',
+            position: {
+                left: '48%',
+                top: '79.5%',
+                transform: 'translate(-50%, -50%)'
+            },
+            size: 'w-[15vw] h-[30vh]',
             sizes: '(max-width: 1011px) 100vw, 35vw'
         },
         {
-            src: '/about/colored/algarve.png',
-            bwSrc: '/about/bw/algarve.png',
-            scale: scale8,
-            className: 'w-[15vw] h-[30vh] top-[30vh] -left-[18vw]',
+            bw: '/about/bw/algarve.png',
+            color: '/about/colored/algarve.png',
+            position: {
+                left: '32%',
+                top: '80%',
+                transform: 'translate(-50%, -50%)'
+            },
+            size: 'w-[15vw] h-[30vh]',
             sizes: '(max-width: 1011px) 100vw, 35vw'
         },
         {
-            src: '/about/colored/limoux.png',
-            bwSrc: '/about/bw/limoux.png',
-            scale: scale9,
-            className: 'w-[15vw] h-[20vh] top-[25.5vh] left-[14vw]',
+            bw: '/about/bw/limoux.png',
+            color: '/about/colored/limoux.png',
+            position: {
+                left: '64%',
+                top: '75.5%',
+                transform: 'translate(-50%, -50%)'
+            },
+            size: 'w-[15vw] h-[20vh]',
             sizes: '(max-width: 1011px) 100vw, 35vw'
         }
-    ]
-
-    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+    ];
 
     return (
-      <>
-        {/* Desktop View */}
-        <div className="hidden lg:block">
-            <div ref={container} className="h-[300vh] relative">
-                <div className="sticky top-0 h-screen overflow-hidden">
-                    {pictures.map(({src, bwSrc, scale, className, sizes}, index) => (
+        <>
+            {/* Desktop View */}
+            <div className="hidden lg:block">
+                <div ref={container} className="h-[300vh] relative">
+                    <div className="sticky top-0 h-screen overflow-hidden">
                         <motion.div 
-                            key={index} 
-                            style={{scale}} 
-                            className="absolute w-full h-full top-0 flex items-center justify-center"
+                            className="absolute inset-0 flex items-center justify-center"
+                            style={{ scale }}
                         >
-                            <div 
-                                className={`relative ${className}`}
-                                style={{
-                                    zIndex: hoveredIndex === index ? 50 : 10,
-                                    pointerEvents: 'auto'
-                                }}
-                                onMouseOver={() => setHoveredIndex(index)}
-                                onMouseOut={() => setHoveredIndex(null)}
-                            >
-                                {/* Black and White Image */}
-                                <Image
-                                    src={bwSrc}
-                                    fill
-                                    alt="image"
-                                    className="object-cover transition-opacity duration-1000"
-                                    style={{
-                                        opacity: hoveredIndex === index ? 0 : 1
-                                    }}
-                                    sizes={sizes}
-                                />
-                                {/* Colored Image */}
-                                <Image
-                                    src={src}
-                                    fill
-                                    alt="image"
-                                    className="object-cover transition-opacity duration-1000"
-                                    style={{
-                                        opacity: hoveredIndex === index ? 1 : 0
-                                    }}
-                                    sizes={sizes}
-                                />
+                            {/* Center point container */}
+                            <div className="relative w-[100vw] h-[100vh]">
+                                {images.map((image, index) => (
+                                    <div
+                                        key={index}
+                                        className={`absolute ${image.size}`}
+                                        style={{
+                                            zIndex: hoveredIndex === index ? 50 : images.length - index,
+                                            ...image.position
+                                        }}
+                                        onMouseEnter={() => setHoveredIndex(index)}
+                                        onMouseLeave={() => setHoveredIndex(null)}
+                                    >
+                                        <div className="relative w-full h-full">
+                                            {/* Black and White Image */}
+                                            <Image
+                                                src={image.bw}
+                                                fill
+                                                alt="image"
+                                                className="object-cover transition-opacity duration-1000"
+                                                style={{
+                                                    opacity: hoveredIndex === index ? 0 : 1
+                                                }}
+                                                sizes={image.sizes}
+                                            />
+                                            {/* Colored Image */}
+                                            <Image
+                                                src={image.color}
+                                                fill
+                                                alt="image"
+                                                className="object-cover transition-opacity duration-1000"
+                                                style={{
+                                                    opacity: hoveredIndex === index ? 1 : 0
+                                                }}
+                                                sizes={image.sizes}
+                                            />
+                                        </div>
+                                    </div>
+                                ))}
                             </div>
                         </motion.div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Mobile View */}
+            <div className="block lg:hidden">
+                <div className="grid grid-cols-2 gap-4 px-4">
+                    {images.map((image, index) => (
+                        <div 
+                            key={index} 
+                            className={`relative z-20 ${
+                                index === 0 ? 'col-span-2 aspect-video' : 'aspect-square'
+                            } w-full`}
+                            onClick={() => setHoveredIndex(index)}
+                        >
+                            <Image
+                                src={image.bw}
+                                fill
+                                alt="image"
+                                className="object-cover transition-opacity duration-500"
+                                style={{
+                                    opacity: hoveredIndex === index ? 0 : 1
+                                }}
+                                sizes="(max-width: 768px) 100vw, 50vw"
+                            />
+                            <Image
+                                src={image.color}
+                                fill
+                                alt="image"
+                                className="object-cover transition-opacity duration-500"
+                                style={{
+                                    opacity: hoveredIndex === index ? 1 : 0
+                                }}
+                                sizes="(max-width: 768px) 100vw, 50vw"
+                            />
+                        </div>
                     ))}
                 </div>
             </div>
-        </div>
-
-        {/* Mobile View */}
-        <div className="block lg:hidden">
-            <div className="grid grid-cols-2 gap-4 px-4">
-                {pictures.map(({src, bwSrc}, index) => (
-                    <div 
-                        key={index} 
-                        className={`relative z-20 ${
-                            index === 0 ? 'col-span-2 aspect-video' : 'aspect-square'
-                        } w-full`}
-                        onClick={() => setHoveredIndex(index)}
-                    >
-                        {/* Black and White Image */}
-                        <Image
-                            src={bwSrc}
-                            fill
-                            alt="image"
-                            className="object-cover transition-opacity duration-500"
-                            style={{
-                                opacity: hoveredIndex === index ? 0 : 1
-                            }}
-                            sizes="(max-width: 768px) 100vw, 50vw"
-                        />
-                        {/* Colored Image */}
-                        <Image
-                            src={src}
-                            fill
-                            alt="image"
-                            className="object-cover transition-opacity duration-500"
-                            style={{
-                                opacity: hoveredIndex === index ? 1 : 0
-                            }}
-                            sizes="(max-width: 768px) 100vw, 50vw"
-                        />
-                    </div>
-                ))}
-            </div>
-        </div>
-    </>
-  )
+        </>
+    );
 }
