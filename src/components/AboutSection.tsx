e4fee8bc37d9ca4829320ef3258f891c5c8cd25b
devStr0ke <samueltomar@hotmail.com>
2025-11-28 11:10:38 +0100

    update : about section translation

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -2,7 +2,7 @@
 import Character from '@/components/Character';
 import { useTranslations } from '@/i18n/LanguageContext';
 
-const paragraph = [
+const englishParagraph = [
   { text: "I'm" },
   { text: "a" },
   { text: "skilled developer", color: "rgb(99 102 241)" }, // Indigo-600
@@ -21,8 +21,35 @@ const paragraph = [
   { text: "web3." }
 ];
 
+const frenchParagraph = [
+  { text: "Je" },
+  { text: "suis" },
+  { text: "un" },
+  { text: "développeur expérimenté", color: "rgb(99 102 241)" },
+  { text: "passionné" },
+  { text: "par" },
+  { text: "la" },
+  { text: "création" },
+  { text: "de" },
+  { text: "solutions" },
+  { text: "web" },
+  { text: "efficaces" },
+  { text: "et" },
+  { text: "évolutives," },
+  { text: "ainsi" },
+  { text: "que" },
+  { text: "par" },
+  { text: "l’exploration" },
+  { text: "du" },
+  { text: "potentiel" },
+  { text: "du" },
+  { text: "web3." }
+];
+
 export const AboutSection = () => {
-  const { t } = useTranslations();
+  const { t, locale } = useTranslations();
+  const paragraph = locale === 'fr' ? frenchParagraph : englishParagraph;
+  
   return (
     <main id="about" className='flex justify-center items-center min-h-min'>
       <Character paragraph={paragraph} title={t.pages.about} />
